d8c42b2c21deab0a6f207071b9da191938189cfe
Heaven <heaven@HeavendeMacBook-Air.local>
2020-08-27 02:06:11 +0800

    xi修改页面

--- App.js
+++ App.js
@@ -32,7 +32,7 @@ const App: () => React$Node = () => {
         <ScrollView
           contentInsetAdjustmentBehavior="automatic"
           style={styles.scrollView}>
-          {/* <Header /> */}
+          <Header />
           {global.HermesInternal == null ? null : (
             <View style={styles.engine}>
               <Text style={styles.footer}>Engine: Hermes</Text>
